a678d35710e9dce9dbe86b51ef3eff130815ea9b
Aishwarya Sadana <34522084+aishsadana@users.noreply.github.com>
2024-09-03 20:14:12 +0000

    scroll

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-$(document).on("click",function(){
+$(document).on("scroll",function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor zaroor aana');
 });
